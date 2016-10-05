b81b678c4d6a4e8c059f6e4418d8f9ed8b9e3bfa
DVerweij <DVerweij@users.noreply.github.com>
2016-10-05 14:13:49 +0200

    Echo function erbij

--- lab5-yourname.py
+++ lab5-yourname.py
@@ -38,7 +38,7 @@ def main(mcast_addr,
     mreq = struct.pack('4sl', inet_aton(mcast_addr[0]), INADDR_ANY)
     mcast.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
     if sys.platform == 'win32': # windows special case
-        mcast.bind( ('localhost', mcast_addr[1]) )
+        mcast.bind( ("", mcast_addr[1]) )
     else: # should work for everything else
         mcast.bind(mcast_addr)
 
@@ -49,7 +49,7 @@ def main(mcast_addr,
     peer.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 5)
     # Bind the socket to a random port.
     if sys.platform == 'win32': # windows special case
-        peer.bind( ('localhost', INADDR_ANY) )
+        peer.bind( ("", INADDR_ANY) )
     else: # should work for everything else
         peer.bind( ('', INADDR_ANY) )
 
@@ -61,16 +61,24 @@ def main(mcast_addr,
     window.writeln( 'my sensor value is %s' % sensor_val )
 
     global neighbors
+    global echocnt
+    echoct = 0
+    global echoreplies
+    echoreplies = 0
+    global initiationnode
+    initiationnode = False
     neighbordiscovery(peer)
+    print neighbors
     # -- This is the event loop. --
     while window.update():
         # Selector module, readytowrite = neighbors
-        readytoread, readytowrite, error = select.select([mcast, peer], [peer], [])
+        readytoread, readytowrite, error = select.select([mcast, peer], [peer], [], 0)
         for i in readytoread:
             message, addres = i.recvfrom(2048)
             a = peer.getsockname()[1]
             # somehow multicast send message to itsself too
             if (addres[1] != a):
+                print message_decode(message)
                 type, sequence, (ix, iy), (nx, ny), operation, capability, payload = message_decode(message)
                 # received a ping message.
                 if type == 0:
@@ -78,10 +86,65 @@ def main(mcast_addr,
                     comparerange(ix, iy, addres, peer)
 
                 # pong message
-                if type == 1:
+                elif type == 1:
                     print "received pong"
                     neighbors[addres] = (nx,ny)
-                    print neighbors
+                
+                elif type == 2:
+                    print "received echo"
+                    echocnt += 1
+                    echoReceive(peer, addres, (ix, iy), (nx, ny))
+                    
+                
+                elif type == 3:
+                    print "recieved echo_reply"
+                    echoreplies += 1
+                    echoReply(peer, (ix, iy))
+                    
+                    
+                    
+def echoSend(peer, initiator, neighbor):
+    global initiationnode
+    if pos == initiator:
+        initiationnode = True
+    global neighbors
+    message = message.encode(MSG_ECHO,0, initiator, neighbor, OP_NOOP)
+    for addres in neighbors:
+        peer.sendto(message, addres)
+        
+    
+def echoReceive(peer, addres, initiator, neighbor):
+    global echocnt
+    global father
+    father = addres # save father
+    global neighbors
+    if echocnt == 1: 
+        if len(neighbors) == 1:
+            message = message.encode(MSG_ECHO_REPLY,0,pos,initiator,OP_NOOP)
+            peer.sendto(message, addres)
+        else:
+            echoSend(peer, initiator, neighbor)
+    else:
+        message = message.encode(MSG_ECHO_REPLY,0,pos,initiator,OP_NOOP)
+        peer.sendto(message, addres)
+        
+def echoReply(peer, initiator):
+    global echoreplies
+    global initiationnode
+    global neighbors
+    if len(neighbors) == echoreplies:
+        if initiationnode == False:
+            global father
+            message = message.encode(MSG_ECHO_REPLY,0,pos,initiator,OP_NOOP)
+            peer.sendto(message, father)
+        else:
+            print "echo successful"
+    
+    
+        
+    
+    
+    
 
 
 
@@ -90,7 +153,7 @@ def comparerange(xi, yi, addres, peer):
     if ((pos[0] - xi) ** 2 + (pos[1] - yi)**2) ** 0.5 < args.range:
         message = message_encode(1,0,(xi,yi),pos)
         print "send pong"
-        peer.sendto(message,addres)
+        peer.sendto(message, addres)
 
 
 def neighbordiscovery(peer):
