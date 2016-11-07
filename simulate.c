1009259b044ddbd0540496a81fa8e9db47daf5c9
neverbeam <martijndortmond@hotmail.com>
2016-11-07 13:37:41 +0100

    Update simulate.c

--- simulate.c
+++ simulate.c
@@ -12,25 +12,20 @@
 
 pthread_cond_t count;
 pthread_mutex_t count_mutex;
+pthread_mutex_t printie;
 
 struct parameters {
-int counter;
-int simulationtime;
-double* old;
-double* current;
-double* next;
+    int counter;
+    int simulationtime;
+    int I;
+    double* old;
+    double* current;
+    double* next;
 }params;
 
-/* Add any functions you may need (like a worker) here. */
 void *HelloWorld(void *args);
 
-/* function we want to complete */
-
 /*
- * Executes the entire simulation.
- *
- * Implement your code here.
- *
  * i_max: how many data points are on a single wave
  * t_max: how many iterations the simulation should run
  * num_threads: how many threads to use (excluding the main threads)
@@ -44,6 +39,7 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 
     params.counter = 0;
     params.simulationtime = t_max;
+    params.I = (i_max - 2) / num_threads;
     params.old = old_array;
     params.current = current_array;
     params.next = next_array;
@@ -51,17 +47,21 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
     pthread_t  thread_ids[num_threads];
     pthread_mutex_init(&count_mutex, NULL);
     pthread_cond_init (&count, NULL);
+    pthread_mutex_init(&printie, NULL);
 
     //void *result;
     for (int i = 0; i < num_threads; i++) {
-        int point_per_thread = i * i_max / num_threads;
-        pthread_create( &thread_ids[i], NULL ,&HelloWorld , &point_per_thread);
+        int point_per_thread = i * (i_max -2) / num_threads;
+        printf("points per thread, %d ", point_per_thread);
+        int* num = (int*)  malloc( sizeof( int));
+        *num = point_per_thread;
+        pthread_create( &thread_ids[i], NULL ,&HelloWorld , num);
     }
 
     for(int i = 0; i < t_max; i++) {
         /* wait till all threads are finished the timestep */
         while (params.counter < num_threads) {
-            sleep (1);
+            sleep(0.01);
         }
         /* swap the buffers around */
         double* temp1;
@@ -91,28 +91,20 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 }
 
 void *HelloWorld(void *args) {
-    int* maxi = (int*) args;
-    for (int i = 0; i < params.simulationtime; i++) {
+    int maxi = *((int*)args);
+    double nexti, previousi;
+    printf( "inside hello %d ", maxi);
+    for (int t = 0; t < params.simulationtime; t++) {
         /* bereken en wacht op volgende stap ( nu ff printen dit is een thread) */
             const int c = 0.15;
-            for(int i = 0; i < *maxi; i++) {
-                int nexti, previousi;
-                double eqone = 2 * (params.current[i] - params.old[i]);
-                if(i - 1 < 0) {
-                    previousi = 0;
-                } else {
-                    previousi = params.current[i - 1];
-                }
-                if (i + 1 >= *maxi) {
-                    nexti = 0;
-                } else {
-                    nexti = params.current[i + 1];
-                }
-                double eqtwo = c * (previousi - 2 * (params.current[i] - nexti));
+            for(int i = maxi + 1; i < maxi + params.I; i++) {
+                double eqone = 2 * params.current[i] - params.old[i];
+                previousi = params.current[i - 1];
+                nexti = params.current[i + 1];
+                double eqtwo = c * (previousi - (2 * params.current[i] - nexti));
                 params.next[i] = eqone + eqtwo;
                 printf("%f", params.next[i]);
             }
-
         /* Show the main thread that you fisnished this time step. */
         params.counter += 1;
         /* Wait for the message that all threas are finshed.*/
